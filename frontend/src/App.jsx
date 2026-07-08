dd26cea12ab883f89f9170905321dda499e9ad1b
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-07 23:05:04 -0700

    feat: link frontend and backend together
    - Links both the frontend and backend with each other to make to
    let the database seamless collect data.
    - Allows for all of the current applications to be shown by website
    on reload or previous database results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ApplicationForm from './components/ApplicationForm'
 import ApplicationList from './components/ApplicationList'
 import './App.css'
 
+
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [applications, setApplications] = useState([]);
+  const localDBApplications = "http://localhost:8000/applications";
 
-  function handleAddApplication(newApplication) {
-    setApplications(previousJobApplication => [...previousJobApplication, newApplication]);
-    setShowForm(false);
+  useEffect(() => {
+    fetch(localDBApplications)
+    .then(res => res.json())
+    .then(data => setApplications(data))
+  }, []);
+
+  async function handleAddApplication(newApplication) {
+    const response = await fetch(localDBApplications, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newApplication)
+    });
+
+    if(response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setApplications(previousJobApplication => [...previousJobApplication, newApplication]);
+        setShowForm(false);
+    }
   }
 
   return (
