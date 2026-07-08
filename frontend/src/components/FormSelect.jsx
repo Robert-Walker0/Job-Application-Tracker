b0ddb3c95b82a637ab8e455d2bc1c02b432da664
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-07 17:42:51 -0700

    feat: add modal popup to ApplicationForm
    - Added a modal popup for the ApplicationForm to make the page
    clearer for adding job applications
    - Moved last heard from above notes to make a cleaner UI

--- frontend/src/components/FormSelect.jsx
+++ frontend/src/components/FormSelect.jsx
@@ -1,17 +1,20 @@
-
-export default function FormSelect ({label, value, onChange, options, required=false}) {
-    return (
-        <div>
-            <label>{label}</label>
-            <select
-                value={value}
-                onChange={onChange}
-                required={required}
-            >
-                {options.map(option => (
-                    <option key={option} value={option}>{option}</option>
-                ))}
-            </select>
-        </div>
-    )
+export default function FormSelect({
+  label,
+  value,
+  onChange,
+  options,
+  required = false,
+}) {
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <select value={value} onChange={onChange} required={required}>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
