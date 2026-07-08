4693a66edbb97b0889e34971761955d4f8446891
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-07 20:27:45 -0700

    feat: create utility functions for backend
    - Create convert_keys and to_camel_case to simplify the data
    transfer between React and FastAPI

---
+++ backend/utility_functions.py
@@ -0,0 +1,7 @@
+
+def convert_keys(application: dict) ->  dict:
+    return {to_camel_case(key): value for key, value in application.items()}
+
+def to_camel_case(string: str) -> str:
+    components = string.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
