9bdde43bb2e77eae927c8ac779e86c48be53f21d
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-09 11:54:41 -0700

    feat: setup backend environment to use .env files

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from database import initialize_project_databases
 from routes.applications import router
+
+FRONTEND_LOCAL_URL = "https://localhost:5173"
+FRONTEND_REMOTE_URL = os.getenv("FRONTEND_REMOTE_URL", FRONTEND_LOCAL_URL)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,8 +19,8 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://job-application-tracker-front.onrender.com"
+        FRONTEND_LOCAL_URL,
+        FRONTEND_REMOTE_URL
     ],
     allow_methods=["*"],
     allow_headers=["*"],
