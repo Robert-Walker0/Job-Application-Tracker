b23176c13f8c3f1748eacfbf8c2ab26884e469c1
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-08 23:41:32 -0700

    feat: add Render origins to deloy project

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://job-application-tracker-front.onrender.com"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
