406b3d64b885d590e17cfbac6b5b63414e0a3d05
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-11 13:09:47 -0700

    tests: replaced convert_keys with new func name

--- backend/tests/test_utility_functions.py
+++ backend/tests/test_utility_functions.py
@@ -1,20 +1,20 @@
-from utility_functions import to_camel_case, convert_keys
+from utility_functions import to_camel_case, to_camel_case_dict
 
 def test_to_camel_case():
     assert to_camel_case("is_true") == "isTrue"
     assert to_camel_case("job_title") == "jobTitle"
     assert to_camel_case("company") == "company"
 
-def test_convert_keys():
-    assert convert_keys({"is_true", "job_title", "last_heard_from"} == {"isTrue", "jobTitle", "lastHeardFrom"})
-    assert convert_keys({"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"} == {"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"}) 
+def test_to_camel_case_dict():
+    assert to_camel_case_dict({"is_true", "job_title", "last_heard_from"} == {"isTrue", "jobTitle", "lastHeardFrom"})
+    assert to_camel_case_dict({"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"} == {"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"}) 
 
 
-def test_convert_keys():
-    assert convert_keys({"is_true": True, "job_title": "Engineer", "last_heard_from": "2026-07-07"}) == {"isTrue": True, "jobTitle": "Engineer", "lastHeardFrom": "2026-07-07"}
-    assert convert_keys({"is_popcorn_done": False, "current_time_in_microwave": 0, "number_of_pops": 0}) == {"isPopcornDone": False, "currentTimeInMicrowave": 0, "numberOfPops": 0}
+def test_to_camel_case_dict():
+    assert to_camel_case_dict({"is_true": True, "job_title": "Engineer", "last_heard_from": "2026-07-07"}) == {"isTrue": True, "jobTitle": "Engineer", "lastHeardFrom": "2026-07-07"}
+    assert to_camel_case_dict({"is_popcorn_done": False, "current_time_in_microwave": 0, "number_of_pops": 0}) == {"isPopcornDone": False, "currentTimeInMicrowave": 0, "numberOfPops": 0}
 
 
-def test_convert_keys_already_camel():
-    assert convert_keys({"company": "Google", "status": "Applied"}) == {"company": "Google", "status": "Applied"}
-    assert convert_keys({"watermelon": True, "size": "huge", "cut": 3}) == {"watermelon": True, "size": "huge", "cut": 3}
+def test_to_camel_case_dict_already_camel():
+    assert to_camel_case_dict({"company": "Google", "status": "Applied"}) == {"company": "Google", "status": "Applied"}
+    assert to_camel_case_dict({"watermelon": True, "size": "huge", "cut": 3}) == {"watermelon": True, "size": "huge", "cut": 3}
