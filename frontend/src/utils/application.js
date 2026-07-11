822930d2a1a8aacf14a5c09616192e67d5eb1185
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-10 22:23:22 -0700

    feat: add frontend highlights of inactive job apps

--- frontend/src/utils/application.js
+++
@@ -1,8 +0,0 @@
-const INACTIVE_DAYS_THRESHOLD = 14;
-
-export function isInactiveJobApplication(dateApplied) {
-    const applicationDate = new Date(dateApplied);
-    const fourteenDaysAgo = new Date();
-    fourteenDaysAgo.setDate(fourteenDaysAgo.getDate() - INACTIVE_DAYS_THRESHOLD);
-    return applicationDate <= fourteenDaysAgo;
-}
