c1da2387f5211b1bead09e57577f4694a780c0de
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-16 11:27:07 -0700

    fix: resolve getting origin crash in CI pipeline

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,4 @@
 import os
-import pytest 
+import pytest
 
-@pytest.fixture(scope="session", autouse=True)
-def setup_test_enviornment():
-    """ 
-    Sets up global environment variables for the entire test session before any application
-    modules are imported.
-    """
-    os.environ["FRONTEND_REMOTE_URL"] = "https://www.example.com"
+os.environ["FRONTEND_REMOTE_URL"] = "https://www.example.com"
