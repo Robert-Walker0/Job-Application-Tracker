f37e66048cc84be6b4caf5a2dc0612c58c97182d
Robert Walker <algorithmicrealmist@gmail.com>
2026-07-15 18:23:54 -0700

    test: fix set assertions for to_camel_dict
    - Rewrote to_camel_case_dict as it was checking a Set dataset
    instead of a dict
    - Reformatted the document using Python Black Formatter in VSC

--- backend/tests/test_utility_functions.py
+++ backend/tests/test_utility_functions.py
@@ -1,20 +1,37 @@
 from utility_functions import to_camel_case, to_camel_case_dict
 
+
 def test_to_camel_case():
     assert to_camel_case("is_true") == "isTrue"
     assert to_camel_case("job_title") == "jobTitle"
     assert to_camel_case("company") == "company"
 
+
 def test_to_camel_case_dict():
-    assert to_camel_case_dict({"is_true", "job_title", "last_heard_from"} == {"isTrue", "jobTitle", "lastHeardFrom"})
-    assert to_camel_case_dict({"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"} == {"isAlreadyCamel", "isBrokenLaptop", "isPaintedAlready"}) 
+    assert to_camel_case_dict(
+        {"is_true": True, "job_title": "Cashier", "last_heard_from": "2026-07-06"}
+    ) == {"isTrue": True, "jobTitle": "Cashier", "lastHeardFrom": "2026-07-06"}
+    assert to_camel_case_dict(
+        {"isAlreadyCamel": True, "isBrokenLaptop": False, "isPaintedAlready": False}
+    ) == {"isAlreadyCamel": True, "isBrokenLaptop": False, "isPaintedAlready": False}
 
 
 def test_to_camel_case_dict():
-    assert to_camel_case_dict({"is_true": True, "job_title": "Engineer", "last_heard_from": "2026-07-07"}) == {"isTrue": True, "jobTitle": "Engineer", "lastHeardFrom": "2026-07-07"}
-    assert to_camel_case_dict({"is_popcorn_done": False, "current_time_in_microwave": 0, "number_of_pops": 0}) == {"isPopcornDone": False, "currentTimeInMicrowave": 0, "numberOfPops": 0}
+    assert to_camel_case_dict(
+        {"is_true": True, "job_title": "Engineer", "last_heard_from": "2026-07-07"}
+    ) == {"isTrue": True, "jobTitle": "Engineer", "lastHeardFrom": "2026-07-07"}
+    assert to_camel_case_dict(
+        {"is_popcorn_done": False, "current_time_in_microwave": 0, "number_of_pops": 0}
+    ) == {"isPopcornDone": False, "currentTimeInMicrowave": 0, "numberOfPops": 0}
 
 
 def test_to_camel_case_dict_already_camel():
-    assert to_camel_case_dict({"company": "Google", "status": "Applied"}) == {"company": "Google", "status": "Applied"}
-    assert to_camel_case_dict({"watermelon": True, "size": "huge", "cut": 3}) == {"watermelon": True, "size": "huge", "cut": 3}
+    assert to_camel_case_dict({"company": "Google", "status": "Applied"}) == {
+        "company": "Google",
+        "status": "Applied",
+    }
+    assert to_camel_case_dict({"watermelon": True, "size": "huge", "cut": 3}) == {
+        "watermelon": True,
+        "size": "huge",
+        "cut": 3,
+    }
